87c4acb32c097dd9cdf19c882569ac5a278f2014
4nth0ny1 <acatullo4@gmail.com>
2023-07-02 17:07:27 -0500

    addTask

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,15 +1,34 @@
 import { useStore } from "../store";
+import { useState } from "react";
 import "./Column.css";
 import Task from "./Task";
 
 export default function Column({ state }) {
+  const [text, setText] = useState("");
   const tasks = useStore((store) =>
     store.tasks.filter((task) => task.state === state)
   );
 
+  const addTask = useStore((store) => store.addTask);
+
   return (
     <div className="column">
-      <p>{state}</p>
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <input
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          type="text"
+        />
+        <button
+          onClick={() => {
+            addTask(text, state);
+            setText("");
+          }}
+        >
+          add
+        </button>
+      </div>
       {tasks.map((task) => (
         <Task title={task.title} key={task.title} />
       ))}
