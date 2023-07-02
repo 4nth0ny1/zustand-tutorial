40e299b34e7c790feee7fb0e1df1d88ea92b4b62
4nth0ny1 <acatullo4@gmail.com>
2023-07-02 15:55:24 -0500

    final before zustand

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,15 @@
+import classNames from "classnames";
 import "./Task.css";
+
+const STATUS = "ONGOING";
 
 export default function Task({ title }) {
   return (
     <div className="task">
       <div>{title}</div>
-      <div>
+      <div className="bottomWrapper">
         <div></div>
-        <div className="status">{status}</div>
+        <div className={classNames("status", STATUS)}>{STATUS}</div>
       </div>
     </div>
   );
