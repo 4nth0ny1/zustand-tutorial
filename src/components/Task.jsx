29b7b57a983a756bdecd7b59a8972f2ef7dbc4e4
4nth0ny1 <acatullo4@gmail.com>
2023-07-02 17:15:23 -0500

    deleteTask

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -6,11 +6,16 @@ export default function Task({ title }) {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
+
+  const deleteTask = useStore((store) => store.deleteTask);
+
   return (
     <div className="task">
       <div>{task.title}</div>
       <div className="bottomWrapper">
-        <div></div>
+        <div>
+          <button onClick={() => deleteTask(task.title)}>del</button>
+        </div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
     </div>
